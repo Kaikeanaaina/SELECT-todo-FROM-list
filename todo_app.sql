b54ad37fb553dbdf4d0f2d6e334c50c0b5ffd09e
KaiKeanaaina <kaikeanaaina@gmail.com>
2016-01-20 09:54:10 -1000

    drop databse name todo_app if it exists

--- todo_app.sql
+++ todo_app.sql
@@ -1,17 +1,11 @@
 DROP user IF EXISTS "michael";
 
 CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
-
-
-
-
-
-
 -- VALUES ('Michael', HASHBYTES('SHA2_512', 'stonebreaker'));
 
--- DROP DATABASE todo_app;
+DROP DATABASE IF EXISTS todo_app;
 
--- CREATE DATABASE todo_app;
+--CREATE DATABASE todo_app;
 
 -- \c todo_app;
 
